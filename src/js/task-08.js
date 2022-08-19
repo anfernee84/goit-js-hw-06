946f79fdde1a62b32f3f12aeb0c62b0406a9fe78
anfernee84 <eduard.rogulin@outlook.com>
2022-08-19 14:07:59 +0300

    Update task-08.js

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,15 @@
+const checkForm = document.querySelector(".login-form");
+console.log(checkForm);
 
+checkForm.addEventListener("submit", handleSubmit);
+function handleSubmit(event) {
+  event.preventDefault();
+  const email = event.currentTarget.elements.email.value;
+  const pass = event.currentTarget.elements.password.value;
+  if (!email || !pass) {
+    alert("All credentials fill you must...");
+  } else {
+    console.log({ email, pass });
+    event.currentTarget.reset();
+  }
+}
