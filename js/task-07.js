ea802e41469c84e7aa5d0cde5433ee84074c2d19
anfernee84 <eduard.rogulin@outlook.com>
2022-08-21 12:15:45 +0300

    Update task-07.js

--- js/task-07.js
+++ js/task-07.js
@@ -2,13 +2,14 @@ const slider = document.querySelector(`#font-size-control`);
 const span = document.querySelector("#text");
 
 /////////////////////////////////////////////////////////// варіант 1
-span.style.fontSize = `${slider.value}px`;
-slider.addEventListener("input", function () {
-  span.style.fontSize = `${slider.value}px`;
-});
+// span.style.fontSize = `${slider.value}px`;
+// slider.addEventListener("input", function () {
+//   span.style.fontSize = `${slider.value}px`;
+// });
 
 ////////////////////////////////////////////////////////// варіант 2
-// slider.addEventListener(
-//   "mousemove",
-//   () => (span.style.fontSize = `${slider.value}px`)
-// );
+span.style.fontSize = `${slider.value}px`;
+slider.addEventListener(
+  "mousemove",
+  () => (span.style.fontSize = `${slider.value}px`)
+);
